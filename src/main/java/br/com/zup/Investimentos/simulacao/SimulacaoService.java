4c31279a9456312c3905e72fda5c523b1ec8eb75
alefe.nunes <alefe.nunes@zup.com.br>
2021-09-02 15:53:36 -0300

    Criar método para fazer simulação

--- src/main/java/br/com/zup/Investimentos/simulacao/SimulacaoService.java
+++ src/main/java/br/com/zup/Investimentos/simulacao/SimulacaoService.java
@@ -1,6 +1,21 @@
 package br.com.zup.Investimentos.simulacao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SimulacaoService {
+
+    @Autowired
+    private SimulacaoRepository simulacaoRepository;
+
+    public Simulacao fazerSimulacao (Simulacao simulacao) {
+       return simulacaoRepository.save(simulacao);
+    }
+
+
+
+
 
 
 
