9fae04d40482ed1a6804a4e672e22f539c4430fa
alefe.nunes <alefe.nunes@zup.com.br>
2021-09-02 15:00:28 -0300

    Implementar Simulação Repository

---
+++ src/main/java/br/com/zup/Investimentos/simulacao/SimulacaoRepository.java
@@ -0,0 +1,8 @@
+package br.com.zup.Investimentos.simulacao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SimulacaoRepository extends CrudRepository<Simulacao, String> {
+
+
+}
