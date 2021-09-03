ba3fbf75049388bb64bd3759a8c25758938aadaa
alefe.nunes <alefe.nunes@zup.com.br>
2021-09-03 17:54:28 -0300

    Implantar método de teste da Service

--- src/test/java/br/com/zup/Investimentos/SimulacaoServiceTest.java
+++ src/test/java/br/com/zup/Investimentos/SimulacaoServiceTest.java
@@ -1,8 +1,11 @@
 package br.com.zup.Investimentos;
 
+import br.com.zup.Investimentos.simulacao.Simulacao;
 import br.com.zup.Investimentos.simulacao.SimulacaoRepository;
 import br.com.zup.Investimentos.simulacao.SimulacaoService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -17,7 +20,31 @@ public class SimulacaoServiceTest {
     private SimulacaoRepository simulacaoRepository;
 
     @Test
-    public void testarMetodoFazerSimulacao
+    public void testarMetodoFazerSimulacaoCaminhoPositivo () {
+
+        Simulacao simulacao = new Simulacao();
+        simulacao.setDinheiroInvestido(1000.00);
+        simulacao.setMesesDoInvestimento(12);
+
+        Mockito.when(simulacaoRepository.save(Mockito.any(Simulacao.class)))
+                .thenReturn(simulacao);
+
+        Assertions.assertEquals(60.00, simulacaoService.fazerSimulacao(simulacao));
+    }
+
+    @Test
+    public void testarMetodoCalcularInvestimentoCaminhoPositivo () {
+
+        Simulacao simulacao = new Simulacao();
+        simulacao.setDinheiroInvestido(1000.00);
+        simulacao.setMesesDoInvestimento(12);
+
+        Mockito.when(simulacaoRepository.save(Mockito.any(Simulacao.class)))
+                .thenReturn(simulacao);
+
+        Assertions.assertEquals(60.00, simulacaoService.calcularInvestimento(simulacao));
+    }
+
 
 
 
